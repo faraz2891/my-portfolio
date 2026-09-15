d4a7a16f268e63b2c35c7232db4f8ed6cb1ebc31
faraz2891 <faraz2891@gmail.com>
2026-09-15 15:25:58 +0530

    update vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/next';
 import './globals.css';
 
 const geistSans = Geist({
@@ -26,6 +27,7 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
       >
          <body suppressHydrationWarning className="min-h-full flex flex-col">
             {children}
+            <Analytics />
          </body>
       </html>
    );
